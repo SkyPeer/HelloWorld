6befca1276ef80473bf41213eadf30c1871c32da
Oleg <o.selivancev@gmail.com>
2017-06-24 23:14:03 +0300

    Task2 Part2 ready

--- task2_2.js
+++ task2_2.js
@@ -1,3 +1,5 @@
-/**
- * Created by admin on 24.06.2017.
- */
+console.log("Task2 Part2");
+for (i=0; i < task2array.length; i++) {
+    var a = task2array[i] + " 1";
+    console.log("i= " + i, "item = " + a)
+}
