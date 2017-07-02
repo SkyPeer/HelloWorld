70208939e9021e1e2ba66678ad10fc4902aa412b
Oleg <o.selivancev@gmail.com>
2017-07-03 01:35:09 +0300

    indexjs1
    0307

---
+++ index.js
@@ -0,0 +1,3 @@
+let array3 = require("./task3.js");
+console.log("Array Length= " + array3.rtask3array.length);
+console.log(array3.rtask3array);
--- task3.js
+++ task3.js
@@ -1,33 +1,44 @@
 let data = require("./arrays");
-console.log(" - - - - - - - - - - - - - - - - - - - - - - - - -");
+
+/* console.log(" - - - - - - - - - - - - - - - - - - - - - - - - -");
 console.log("Task3");
 console.log("Source:");
 console.log(data.task3array);
 console.log(" ");
+console.log("filter:"); */
 
-console.log("filter:");
-var ftask3array = data.task3array.filter(function (user) {
+let ftask3array = data.task3array.filter(function (user) {
     return user.Age >= 18;
 });
-console.log(ftask3array);
+//console.log(ftask3array);
 
 
-console.log(" ");
-console.log("map:");
-rtask3array = ftask3array.map(function (user) {
+//console.log(" ");
+//console.log("map:");
+let rtask3array = ftask3array.map(function (user) {
     var fioarray = {fio: user.FirstName + " " + user.LastName + " " + user.Gender + " ", adult: true};
-    console.log(fioarray);
+    //console.log(fioarray);
     return fioarray;
 });
-console.log(" ");
+
+module.exports =
+    {
+        rtask3array,
+    };
+
+
+/* console.log(" ");
 console.log("FioArray");
 console.log("Array Length= " + rtask3array.length);
-console.log(rtask3array);
+console.log(rtask3array); */
 
-/* var fioarray = [];
+
+
+/*var fioarray = [];
 data.task3array.forEach(item=> {
-    item.fio = `f:${item.FirstName} n:${item.LastName} g:${item.Gender}` ;
-    item.test = {'ttt':2};
-    fioarray = {fio: item.FirstName + " " + item.LastName + " " + item.Gender + " ", adult: true};
-    console.log(item);
-});console.log(data.task3array); */
+    //item.fio = `${item.FirstName + " " + 1} ${item.LastName} ${item.Gender}` ;
+    //item.test = {'ttt':2};
+  //  fioarray = {fio: item.FirstName + " " + item.LastName + " " + item.Gender + " ", adult: true};
+//    console.log(item);
+});
+console.log(data.task3array); */
