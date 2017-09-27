72aad9d804a3ff37ebe8dbac9467b4d490634c27
Oleg <o.selivancev@gmail.com>
2017-09-28 00:44:01 +0300

    2809 HW

--- promise2.js
+++ promise2.js
@@ -1,3 +1,5 @@
+'use strict';
+
 let readline = require('readline');
 
 let rl = readline.createInterface({
@@ -6,17 +8,30 @@ let rl = readline.createInterface({
 });
 
 
-let promise = new Promise((resolve, reject) => {
-    function answefunc(a) {
-        let b = a * 3.25;
-        console.log("Thank you for your valuable feedback:", parseInt(b));
+ function answefunc(a) {
+     let promise = new Promise((result, error) => {
+         let b = a * 3.25;
+         console.log(b);
+     });
+     promise
+         .then(
+             result => {
+                 // первая функция-обработчик - запустится при вызове resolve
+                 console.log("Fulfilled: " + result); // result - аргумент resolve
+             },
+             error => {
+                 // вторая функция - запустится при вызове reject
+                 console.log("Rejected: " + error); // error - аргумент reject
+             }
+         );
+     console.log(promise);
+     return promise;
 
-        /* return answer; */
-    }
-});
+ }
 
     rl.question("What do you think of io.js? ", function (answer) {
         answefunc(answer);
+
         rl.close();
     });
 
